58270d5b01a6bb14cd73f695de3c183857b9c4ea
Alexey <alexlitv1977@gmail.com>
2026-09-22 00:54:50 +0500

    add first criterion

---
+++ OG04.py
@@ -0,0 +1,7 @@
+def ravenstvo_po_dvum_storonam_i_uglu(a1, b1, ugol1, a2, b2, ugol2):
+    if a1 == a2 and b1 == b2 and ugol1 == ugol2:
+        print("Треугольники равны по двум сторонам и углу между ними.")
+    else:
+        print("По данному признаку равенство треугольников не доказано.")
+        
+ravenstvo_po_dvum_storonam_i_uglu(5, 7, 60, 5, 7, 60)
